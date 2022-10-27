7899e65c942d2eff897637bf5303113e280f018a
rookie <rookie@taou.com>
2022-10-27 15:57:46 +0800

    default_capibilities

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
 require("basic")
 require("lsp")
 require("keys")
--- lua/lsp/nvim-lsp.lua
+++ lua/lsp/nvim-lsp.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 local C = {}
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 function C.capabilities()
     return capabilities
