351f66b4efa286d13cdc4f41ac622fea91bf8e64
rookie <xpc-lx@hotmail.com>
2024-02-26 16:30:45 +0800

    packer --> lazy

--- init.lua
+++ init.lua
@@ -2,6 +2,21 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 require("plugins")
 require("basic")
 require("lsp")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,111 +1,102 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-    use "williamboman/mason.nvim" -- lsp installer
-    use 'airblade/vim-gitgutter' -- git
+local plugins = {
+    {'neovim/nvim-lspconfig'}, -- Configurations for Nvim LSP
+    {"williamboman/mason.nvim"}, -- lsp installer
+    {'airblade/vim-gitgutter'}, -- git
 
     -- completions
-    -- use {'neoclide/coc.nvim', branch = 'release'}
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/nvim-cmp'
-    use {
+    -- {'neoclide/coc.nvim', branch = 'release'}
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-cmdline'},
+    {'hrsh7th/cmp-nvim-lua'},
+    {'hrsh7th/nvim-cmp'},
+    {
         'hrsh7th/cmp-vsnip',
-        requires = {
+        dependencies= {
             'hrsh7th/cmp-nvim-lsp',
         }
-    }
-    use 'hrsh7th/vim-vsnip'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'L3MON4D3/LuaSnip'
-    use 'rafamadriz/friendly-snippets'
-    use 'onsails/lspkind-nvim'
+    },
+    {'hrsh7th/vim-vsnip'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
+    {'onsails/lspkind-nvim'},
     -- completions
 
-    use 'j-hui/fidget.nvim' -- Standalone UI for nvim-lsp progress
+    { 'j-hui/fidget.nvim'}, -- Standalone UI for nvim-lsp progress
 
     -- explore
-    use {
+    {
         'nvim-tree/nvim-tree.lua',
-        requires = {
+        dependencies = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-    }
+        }
+    },
 
-    use {
+    {
         'nvim-telescope/telescope.nvim',
         branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+        dependencies = {  'nvim-lua/plenary.nvim' } 
+    },
 
     -- color schema
-    use {
+    {
         "catppuccin/nvim",
-        as = "catppuccin",
-    }
+        name = "catppuccin",
+        priority = 1000
+    },
 
     -- auto pairs
-    use {
+    {
         "windwp/nvim-autopairs",
-    }
+    },
 
     -- treesitter
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-    }
+    },
 
     -- status line
-    use {
+    {
         'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
 
     -- tab line
-    use {
+    {
         'akinsho/bufferline.nvim',
-        tag = "v3.*",
-        requires = 'nvim-tree/nvim-web-devicons'
-    }
+        version = "*",
+        dependencies= {'nvim-tree/nvim-web-devicons'}
+    },
 
     -- cursor
-    use {
+    --[[{
         'RRethy/vim-illuminate'
-    }
+    },]]--
 
     -- diagnostic
-    use {
+    {
         "folke/trouble.nvim",
-        requires = "nvim-tree/nvim-web-devicons",
-    }
+        dependencies= {"nvim-tree/nvim-web-devicons"},
+    },
 
-    use {
+    {
         "github/copilot.vim"
-    }
+    },
 
-    use({
+    {
         "folke/noice.nvim",
-        requires = {
+        event = "VeryLazy",
+        dependencies= {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
             -- OPTIONAL:
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
-            "nvim-tree/nvim-web-devicons",
         }
-    })
-end)
+    },
+}
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
+require('lazy').setup(plugins,{})
