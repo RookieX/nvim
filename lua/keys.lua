05a54bcf6b0926b86c85295b21eaea11c5e67ebb
rookie <rookie@taou.com>
2025-01-23 15:49:57 +0800

    plugin:legendary.nvim

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,22 +1,20 @@
 local set = vim.keymap.set
 local opt = { noremap = true, silent = true }
 
-set("n", "<F1>", ":NvimTreeToggle<CR>", opt)
-set("n", "<F3>", ":NvimTreeFindFile<CR>", opt)
+-- set("n", "<F1>", ":NvimTreeToggle<CR>", opt)
+-- set("n", "<F3>", ":NvimTreeFindFile<CR>", opt)
 
-set("n", "gp", ":BufferLinePick<CR>", opt)
-set("n", "gc", ":BufferLinePickClose<CR>", opt)
-set("n", "gt", ":BufferLineCycleNext<CR>", opt)
-set("n", "gT", ":BufferLineCyclePrev<CR>", opt)
+-- set("n", "gp", ":BufferLinePick<CR>", opt)
+-- set("n", "gq", ":BufferLinePickClose<CR>", opt)
+-- set("n", "gt", ":BufferLineCycleNext<CR>", opt)
+-- set("n", "gT", ":BufferLineCyclePrev<CR>", opt)
 
-set('n', '<C-T>', ":Telescope find_files hidden=true<CR>", opt)
-set('n', '<C-L>', ":Telescope live_grep<CR>", opt)
+-- set('n', '<C-T>', ":Telescope find_files hidden=true<CR>", opt)
+-- set('n', '<C-L>', ":Telescope live_grep<CR>", opt)
 
-set('n', "<C-\\>f", ":ToggleTerm direction=float<CR>", opt)
-set('n', "<C-\\>h", ":ToggleTerm direction=horizontal<CR>", opt)
-set('n', "<C-\\>v", ":ToggleTerm direction=vertical<CR>", opt)
+-- set('n', "<C-\\>f", ":ToggleTerm direction=float<CR>", opt)
+-- set('n', "<C-\\>h", ":ToggleTerm direction=horizontal<CR>", opt)
+-- set('n', "<C-\\>v", ":ToggleTerm direction=vertical<CR>", opt)
 
-set("n", "go", "<cmd>Lspsaga outline<CR>", opt)
-
-set("n", "<leader>se", function() require("scissors").editSnippet() end)
-set({ "n", "x" }, "<leader>sa", function() require("scissors").addNewSnippet() end)
+-- set("n", "<leader>se", function() require("scissors").editSnippet() end)
+-- set({ "n", "x" }, "<leader>sa", function() require("scissors").addNewSnippet() end)
---
+++ lua/plugins/legendary.nvim.lua
@@ -0,0 +1,36 @@
+return {
+    'mrjones2014/legendary.nvim',
+    -- since legendary.nvim handles all your keymaps/commands,
+    -- its recommended to load legendary.nvim before other plugins
+    priority = 10000,
+    lazy = false,
+    -- sqlite is only needed if you want to use frecency sorting
+    -- dependencies = { 'kkharji/sqlite.lua' }
+    opts = {
+        keymaps = {
+            -- NvimTree
+            { '<F1>', ':NvimTreeToggle<CR>', description = 'Toggle NvimTree', opts = { silent = true } },
+            { '<F3>', ':NvimTreeFindFile<CR>', description = 'Find file in NvimTree', opts = { silent = true } },
+
+            -- Telescope
+            { '<C-T>', ':Telescope find_files hidden=true<CR>', description = 'Find files', opts = { silent = true } },
+            { '<C-L>', ':Telescope live_grep<CR>',              description = 'Live Grep',  opts = { silent = true } },
+            { '<leader>k', ':Telescope keymaps<CR>',              description = 'Keymaps',  opts = { silent = true } },
+
+            -- Bufferline
+            { 'gp', ':BufferLinePick<CR>',       description = 'Pick buffer', opts = { silent = true } },
+            { 'gq', ':BufferLinePickClose<CR>', description = 'Close buffer', opts = { silent = true } },
+            { 'gt', ':BufferLineCycleNext<CR>', description = 'Next buffer', opts = { silent = true } },
+            { 'gT', ':BufferLineCyclePrev<CR>', description = 'Prev buffer', opts = { silent = true } },
+
+            -- ToggleTerm
+            { '<leader>f', ':ToggleTerm direction=float<CR>',      description = 'Float terminal', opts = { silent = true } },
+            { '<leader>h', ':ToggleTerm direction=horizontal<CR>', description = 'Horizontal terminal', opts = { silent = true } },
+            { '<leader>v', ':ToggleTerm direction=vertical<CR>',   description = 'Vertical terminal', opts = { silent = true } },
+
+            -- Scissors
+            { '<leader>se', function() require("scissors").editSnippet() end, description = 'Edit snippet', opts = { silent = true } },
+            { '<leader>sa', function() require("scissors").addNewSnippet() end, description = 'Add new snippet', opts = { silent = true } },
+        }
+    }
+}
