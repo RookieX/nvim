8e1b256e5cc7db3eb4008b5725167cafaecf9390
rookie <rookie@taou.com>
2025-01-24 16:19:57 +0800

    remove black

--- lua/lsp/emf.lua
+++ lua/lsp/emf.lua
@@ -1,10 +1,3 @@
 require('lspconfig').efm.setup {
     init_options = { documentFormatting = true },
-    settings = {
-        languages = {
-            python = {
-                { formatCommand = "black --quiet -", formatStdin = true }
-            }
-        }
-    }
 }
