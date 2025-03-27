17ccfc58172ac3999dfd495cab76224c1e10ce79
rookie <rookie@taou.com>
2025-03-27 17:39:28 +0800

    plugin:telescope-fzf-native.nvim

--- lua/plugins/telescope.nvim.lua
+++ lua/plugins/telescope.nvim.lua
@@ -4,7 +4,8 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-telescope/telescope-live-grep-args.nvim',
-        "nvim-telescope/telescope-ui-select.nvim"
+        "nvim-telescope/telescope-ui-select.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
         local actions = require("telescope.actions")
@@ -58,13 +59,20 @@ return {
                     -- theme = { }, -- use own theme spec
                     -- layout_config = { mirror=true }, -- mirror preview pane
                 },
-                ["ui-select"]= {
+                ["ui-select"] = {
                     require("telescope.themes").get_dropdown {
                         -- even more opts
                     },
                     specific_opts = {
                         codeactions = true,
                     },
+                },
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
                 },
                 noice = {}
                 -- Your extension configuration goes here:
@@ -76,5 +84,6 @@ return {
         })
         require("telescope").load_extension("live_grep_args")
         require("telescope").load_extension("ui-select")
+        require("telescope").load_extension("fzf")
     end
 }
