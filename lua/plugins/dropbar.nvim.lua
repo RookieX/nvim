59543ff5f586e54152eadc6d92b3da1eb202827d
rookie <xpc-lx@hotmail.com>
2025-04-09 15:57:40 +0800

    unpin dropbar version

--- lua/plugins/dropbar.nvim.lua
+++ lua/plugins/dropbar.nvim.lua
@@ -1,7 +1,7 @@
 -- breadcrumbs
 return {
     'Bekaboo/dropbar.nvim',
-    tag = 'v12.0.2',
+    -- tag = 'v12.0.2',
     -- optional, but required for fuzzy finder support
     dependencies = {
         'nvim-telescope/telescope-fzf-native.nvim'
