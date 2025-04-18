f92da882e8cc00ed93b47d59ff80583c66516c7b
rookie <xpc-lx@hotmail.com>
2025-04-18 15:43:43 +0800

    tab for copilot

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -39,7 +39,16 @@ return {
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         keymap = {
-            preset = 'enter'
+            preset = 'enter',
+            ['<Tab>'] = { function(cmp)
+                if cmp.snippet_active() then
+                    return cmp.snippet_forward()
+                else
+                    -- return cmp.select_and_accept()
+                    return false
+                end
+            end, 'fallback' },
+
         },
 
         appearance = {
