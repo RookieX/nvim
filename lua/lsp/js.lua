b86e4ab0e384d017d120c8349b5a6c263e4ab7a8
rookie <xpc-lx@hotmail.com>
2025-09-22 11:27:33 +0800

    js lsp

--- lua/lsp/js.lua
+++ lua/lsp/js.lua
@@ -1,3 +1,7 @@
-require('lspconfig').ts_ls.setup {
+vim.lsp.enable('ts_ls')
+
+vim.lsp.config('ts_ls', {
+    on_attach = on_attach,
     capabilities = require("lsp.capabilities").capabilities(),
 }
+)
