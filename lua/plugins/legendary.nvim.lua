376823e9e810eef438cc90b36f12d057b7eb7432
rookie <xpc-lx@hotmail.com>
2026-01-05 15:33:04 +0800

    plugin:flash

--- lua/plugins/legendary.nvim.lua
+++ lua/plugins/legendary.nvim.lua
@@ -67,6 +67,13 @@ return {
 
             -- Oil
             { '<leader>o', ':Oil<CR>', description = 'Oil', opts = { silent = true } },
+
+            -- motion
+            { "<leader>js", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "<leader>jS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "<leader>jR", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         }
     }
 }
